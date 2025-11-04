c36f1dbe9ec726fdda95aeddb4e3422903fba8d0
oohasikazuyuki <cdok1230245@gn.iwasaki.ac.jp>
2025-11-05 08:12:05 +0900

    flashメッセージ表示改善

--- src_web/kamaho-shokusu/templates/TReservationInfo/index.php
+++ src_web/kamaho-shokusu/templates/TReservationInfo/index.php
@@ -1057,7 +1057,7 @@ $JS_CURRENT_ROOM     = json_encode($currentRoomId ?? '', JSON_UNESCAPED_UNICODE|
             toastEl.className = 'toast align-items-center text-bg-' + (type === 'success' ? 'success' : (type === 'warning' ? 'warning' : 'danger')) + ' border-0';
             toastEl.role = 'alert'; toastEl.ariaLive = 'assertive'; toastEl.ariaAtomic = 'true';
             toastEl.innerHTML = `
-        <div class="d-flex>
+        <div class="d-flex">
           <div class="toast-body">${message}</div>
           <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>`;
