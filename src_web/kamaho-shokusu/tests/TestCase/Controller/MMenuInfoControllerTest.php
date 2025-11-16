e7af08ea7a51fa66073dd024f9ce89e020f05ef4
oohasikazuyuki <cdok1230245@gn.iwasaki.ac.jp>
2025-11-16 23:52:03 +0900

    Fix: 本番環境のchangeEdit POST送信時の部屋ID・日付取得エラーを修正
    - changeEditアクションでPOSTデータからもパラメータを取得するように修正
    - i_id_room と d_reservation_date をgetData()で取得
    - 「部屋IDまたは日付が指定されていません」エラーを解消

---
+++ src_web/kamaho-shokusu/tests/TestCase/Controller/MMenuInfoControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\MMenuInfoController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\MMenuInfoController Test Case
+ *
+ * @uses \App\Controller\MMenuInfoController
+ */
+class MMenuInfoControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var list<string>
+     */
+    protected array $fixtures = [
+        'app.MMenuInfo',
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     * @uses \App\Controller\MMenuInfoController::index()
+     */
+    public function testIndex(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     * @uses \App\Controller\MMenuInfoController::view()
+     */
+    public function testView(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     * @uses \App\Controller\MMenuInfoController::add()
+     */
+    public function testAdd(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     * @uses \App\Controller\MMenuInfoController::edit()
+     */
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     * @uses \App\Controller\MMenuInfoController::delete()
+     */
+    public function testDelete(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
