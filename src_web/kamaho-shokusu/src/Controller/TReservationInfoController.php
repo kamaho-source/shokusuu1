e1d85204c8aef2f708bfc9239abb1d30f0f30dd6
oohasikazuyuki <cdok1230245@gn.iwasaki.ac.jp>
2026-04-07 09:52:27 +0900

    fix: #123 TReservationInfo/view アクションで日付パスパラメータを引数で受け取るよう修正
    setPass(['date']) を使うと :date パラメータが pass 配列に移動し、
    getParam('date') では取得できなかった。
    アクション引数 ?string \$date = null で受け取ることで
    CakePHP のルーティング機構が自動的にマッピングし、
    日付が正しく取得されるようになる。

--- src_web/kamaho-shokusu/src/Controller/TReservationInfoController.php
+++ src_web/kamaho-shokusu/src/Controller/TReservationInfoController.php
@@ -289,13 +289,11 @@ class TReservationInfoController extends AppController
      * @throws \Cake\Datasource\Exception\RecordNotFoundException 記録が見つからない場合
      * 管理者および所属部屋のみ詳細閲覧と修正可能
      */
-    public function view(): ?Response
+    public function view(?string $date = null): ?Response
     {
         $this->authorizeReservation('view');
 
-        $date = $this->request->getParam('date')
-            ?? $this->request->getParam('pass.0')
-            ?? $this->request->getQuery('date');
+        $date = $date ?? $this->request->getQuery('date');
         $context = $this->viewService->buildViewContext(
             $this->request->getAttribute('identity'),
             $date,
